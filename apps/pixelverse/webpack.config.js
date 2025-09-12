57a9cd4b9cd9c0e0c59c12c88ae6c6117f0e26bb
pixelverseacademy <vinodh.tankasala@iqvia.com>
2025-09-12 10:52:26 -0400

    fix production build

--- apps/pixelverse/webpack.config.js
+++ apps/pixelverse/webpack.config.js
@@ -21,7 +21,7 @@ module.exports = {
       main: './src/main.tsx',
       index: './src/index.html',
       baseHref: '/',
-      assets: ['./src/favicon.ico', './src/assets'],
+      assets: ['./src/favicon.ico', './src/assets', './public'],
       styles: [],
       outputHashing: process.env['NODE_ENV'] === 'production' ? 'all' : 'none',
       optimization: process.env['NODE_ENV'] === 'production',
